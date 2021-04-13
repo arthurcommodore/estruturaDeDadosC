7e73c2b4b52a9b719a1b0aaadabd6f026acf6629
arhur <arhurdeveloper@gmail.com>
2021-04-13 07:51:48 -0300

    Foi finalizado o exemplo de fila

--- fila.c
+++ fila.c
@@ -19,9 +19,74 @@ void lista_elementos() {
     puts("\n\n");
 }
 
+void enqueue() {
+    int val;
+    if(tail < TAM) {
+        printf("Informe o elemento para adicionar na fila: ");
+        scanf("%d", &val);
+
+        fila[tail] = val;
+        tail += 1;
+    }else
+        printf("A fila está cheia...\n");
+
+}
+
+void dequeue() {
+    if(head < tail) {
+        fila[head] = 0;
+        head += 1; 
+    }else
+        puts("A fila está vazia");
+
+}
+
+void clear() {
+    head = 0;
+    tail = 0;
+    for(int i = 0; i < TAM; i++) {
+        fila[i] = 0;
+    }
+}
+
 int main() {
-    lista_elementos();
+    int option = 0;
+
+    do {
+        puts("Selecione a opção: \n\n");
+        puts("[1] - Inserir (enqueue): \n");
+        puts("[2] - Remover (dequeue): \n");
+        puts("[3] - Listar \n");
+        puts("[4] - Limpra a fila: \n");
+        puts("[-1] - Sair \n");
+        scanf("%d", &option);
 
 
+        switch(option) {
+            case 1:
+                enqueue();
+                break;
+
+            case 2: 
+                dequeue();
+                break;
+
+            case 3:
+                lista_elementos();
+                break;
+
+            case 4:
+                clear();
+                break;
+
+            case -1:
+                break;
+
+            default:
+                puts("opção invalida");
+
+        }
+
+    }while(option != -1);
 
 }
